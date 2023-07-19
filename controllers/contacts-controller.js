3075ed99a07629f3f9c5872d30eb05f641293916
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-07-19 08:23:44 +0300

    fixed

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,19 +1,7 @@
-// import Joi from "joi";
 import contactsService from "../models/contacts.js";
 import {ctrlWrapper} from "../decorators/index.js";
 import { HttpError } from "../helpers/index.js";
 
-/* const contactsAddSchema = Joi.object ({
-    name: Joi.string().required().messages({
-      "any.required": `missing required name field`,
-    }),
-    email: Joi.string().required().messages({
-      "any.required": `missing required email field`,
-    }),
-    phone: Joi.string().required().messages({
-      "any.required": `missing required phone field`,
-    }),
-  }) */
 
   const getAll = async (req, res) => {
         const result = await contactsService.listContacts();
@@ -31,20 +19,14 @@ import { HttpError } from "../helpers/index.js";
     
 
  const add = async (req, res) => {
-      // const {error} = contactsAddSchema.validate(req.body);
-      // if(error) {
-      //   throw HttpError(400, error.message)
-      // }
+      
        const result = await contactsService.addContact(req.body)
        res.status(201).json(result)
     } 
       
 
  const updateById = async (req, res) => {
-      // const {error} = contactsAddSchema.validate(req.body);
-      // if(error) {
-      //   throw HttpError(400, error.message)
-      // }
+     
       const {contactId} = req.params;
       const result = await contactsService.updateContact(contactId, req.body);
       if(!result) {
