a96b8225a69cbc07c6a718d2cfe2823a675d1fbc
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-07-25 12:00:25 +0300

    completed

--- models/contacts.js
+++ models/contacts.js
@@ -1,62 +1,30 @@
-import fs from "fs/promises";
-import path from "path";
-import { nanoid } from "nanoid";
+import { Schema, model } from "mongoose";
+import { handleSaveError, validateAtUpdate } from "./hooks.js";
 
-const contactsPath = path.resolve("models", "contacts.json");
-const updateContactsStorage = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+      favorite: {
+        type: Boolean,
+        default: false,
+      },
+      }, {versionKey: false, timestamps: true} );
 
 
-const listContacts = async () => {
-  const data = await fs.readFile(contactsPath)
-    return JSON.parse(data);
-}
+contactSchema.pre("findOneAndUpdate", validateAtUpdate);
 
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-    const result = contacts.find(item => item.id === contactId);
-    return result || null;
-}
+contactSchema.post("save", handleSaveError);
+contactSchema.post("findOneAndUpdate", handleSaveError);
 
-const removeContact = async(contactId) => {
-   
-  const contacts = await listContacts();
-  const index = contacts.findIndex(item => item.id === contactId);
-  if(index === -1){
-    return null;
-  }
-  const [result] = contacts.splice(index, 1);
-  await updateContactsStorage(contacts);
-  return result;
-}
 
-const addContact = async ({ name, email, phone }) => {
-  const contacts = await listContacts();
-  const newContact = {
-      id: nanoid(),
-      name,
-      email,
-      phone
-  }
-  contacts.push(newContact);
-  await updateContactsStorage(contacts);
-  return newContact;
-}
+const Contact = model("contact", contactSchema);
 
-  const updateContact  = async(id, {name, email, phone}) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex(item => item.id === id);
-  if(index === -1){
-      return null;
-  }
-  contacts[index] = {id, name, email, phone};
-  await updateContactsStorage(contacts);
-  return contacts[index];
-}
-
-export default {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+export default Contact;
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -6,16 +6,20 @@ import contactsSchemas from "../../schemas/contacts-schemas.js";
 
 import {validateBody} from "../../decorators/index.js";
 
+import {isValidId, isEmptyBody} from "../../middlewars/index.js"
+
 const contactsRouter = express.Router()
 
 contactsRouter.get('/', contactsController.getAll)
 
-contactsRouter.get('/:contactId', contactsController.getById)
+contactsRouter.get('/:contactId', isValidId, contactsController.getById)
 
 contactsRouter.post('/', validateBody(contactsSchemas.contactsAddSchema), contactsController.add)
 
-contactsRouter.delete('/:contactId', contactsController.deleteById)
+contactsRouter.delete('/:contactId', isValidId, contactsController.deleteById)
 
-contactsRouter.put('/:contactId', validateBody(contactsSchemas.contactsAddSchema), contactsController.updateById)
+contactsRouter.put('/:contactId', isValidId, validateBody(contactsSchemas.contactsAddSchema), contactsController.updateById)
+
+contactsRouter.patch('/:contactId/favorite', isValidId, isEmptyBody, validateBody(contactsSchemas.contactUpdateFavoriteSchema), contactsController.updateStatusContact)
 
 export default contactsRouter;
